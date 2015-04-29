f708968730d57a51f9687180182763426d54835c
Leonardo Gaboardi <leogaboardi@gmail.com>
2015-04-28 20:08:24 -0500

    Changes in devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
+    :domain         => ENV['mailer_domain'],
+    :user_name      => ENV['mailer_username'],
+    :password       => ENV['mailer_password'],
     :authentication => :plain,
   }
 end
