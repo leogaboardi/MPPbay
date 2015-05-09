bb5e05344692083c91cc432bca31f3c86699845d
Leonardo Gaboardi <leogaboardi@gmail.com>
2015-05-09 17:31:52 -0500

    Paperclip fix

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -9,7 +9,7 @@ class Picture < ActiveRecord::Base
   #:content_type => { :content_type => "image/jpg" },
   #:size => { :in => 0..100.kilobytes }
 
-  Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.0-Q16'
+  #Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.0-Q16'
 
   has_attached_file :image, styles: { thumbnail: "80x80", med: "300x300", large: "500x500" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
